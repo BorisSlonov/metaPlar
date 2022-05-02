86871236bc9f441eadcca3444c34e4033756b875
Boris <sslonovborisss@gmail.com>
2022-05-02 19:35:33 +0300

    add 1 page

---
+++ js/script.js
@@ -0,0 +1,55 @@
+//burger
+window.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".header__list"),
+    menuItem = document.querySelectorAll(".header__link"),
+    hamburger = document.querySelector(".header__burger"),
+    overflowHidden = document.querySelector("body");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("burger_active");
+    menu.classList.toggle("menu_active");
+    overflowHidden.classList.toggle("overflow-hidden-y");
+  });
+
+  menuItem.forEach((item) => {
+    item.addEventListener("click", () => {
+      hamburger.classList.toggle("burger_active");
+      menu.classList.toggle("menu_active");
+      overflowHidden.classList.toggle("overflow-hidden-y");
+    });
+  });
+});
+
+$(document).ready(function () {
+  var $element = $(".block1");
+  let counter = 0;
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    //Если скролл до конца елемента
+    // var offset = $element.offset().top + $element.height();
+    //Если скролл до начала елемента
+    // var offset = $element.offset().top;
+
+    if ($(window).width() <= 1240) {
+      if (scroll <= 60) {
+        $(".header").css({
+          position: "absolute",
+          top: "60px",
+          bottom: "auto",
+        });
+      } else {
+        $(".header").css({ position: "fixed", top: "0", bottom: "auto" });
+      }
+    } else {
+      if (scroll <= 180) {
+        $(".header").css({
+          position: "absolute",
+          top: "180px",
+          bottom: "auto",
+        });
+      } else {
+        $(".header").css({ position: "fixed", top: "0", bottom: "auto" });
+      }
+    }
+  });
+});
